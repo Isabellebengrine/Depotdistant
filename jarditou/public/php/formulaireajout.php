2faed207234752d6d145759b0b009f4063dcdc5d
IsabelleBengrine <ibengrine@yahoo.com>
2020-07-16 11:27:54 +0200

    dernières finitions

--- jarditou/public/php/formulaireajout.php
+++ jarditou/public/php/formulaireajout.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"><!--règle un bug spécifique du navigateur
         Apple Safari mobile sous le système d'exploitation iOS 9 (iPhone/iPad).-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><!--pour importer Bootstrap via une URL pointant sur un CDN (un serveur externe hébergeant des fichiers)-->
-    <link rel="stylesheet" type="text/css" href="public/css/jarditoustyle.css">
-    <link rel="stylesheet" media="screen and (max-width: 768px)" href="public/css/Jstyleresponsive.css">
+    <link rel="stylesheet" media="screen and (max-width: 768px)" href="public/css/style.css" type="text/css">
     <title>Formulaire d'ajout de nouveau produit</title>
   </head>
   <body> 
